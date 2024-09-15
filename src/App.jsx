e2fc48ef36ebd443f1aafc489792a66a4874c000
Sujeet Kumar <sk0990670@gmail.com>
2024-09-16 01:09:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Card from "./components/card";
+
+function App(){
+ 
+
+  return(
+    <>
+      {/* <Card/> */}
+      <Song />
+     <h1>Hey</h1>
+   </>
+
+  )
+}
+export default App;
